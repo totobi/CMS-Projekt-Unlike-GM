9ce317511f50ffd0045e4c3f4d32f467e6c957a4
totobi <tobi.kkoerner@web.de>
2014-11-12 20:44:48 +0100

    firefox bug

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'https://unlike-gm.herokuapp.com/',
+        url: 'http://unlike-gummersbach.de.vu',
         mail: {},
         database: {
             client: 'sqlite3',
